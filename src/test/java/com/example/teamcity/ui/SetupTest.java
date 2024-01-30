eb07dd06ccc60a0a1bc8287a9f30c5f01b0b2a23
WMC\artem.mindsadyrov <artem.mindsadyrov@actumdigital.com>
2024-01-30 13:44:48 +0100

    SetupTest.java added to ui

---
+++ src/test/java/com/example/teamcity/ui/SetupTest.java
@@ -0,0 +1,14 @@
+package com.example.teamcity.ui;
+
+import com.example.teamcity.ui.pages.StartUpPage;
+import org.testng.annotations.Test;
+
+public class SetupTest extends BaseUiTest{
+    @Test
+    public void startUpTest(){
+        new StartUpPage()
+            .open()
+            .setupTeamCityServer();
+            //.getHeader().shouldHave(Condition.text("Create Administrator Account"));
+    }
+}
