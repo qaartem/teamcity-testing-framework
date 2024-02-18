d9d811e01320e7f8156fd82989beb6b32edc2e39
WMC\artem.mindsadyrov <artem.mindsadyrov@actumdigital.com>
2024-02-18 19:04:12 +0100

    fix selectors and methods

--- src/main/java/com/example/teamcity/ui/pages/StartUpPage.java
+++ src/main/java/com/example/teamcity/ui/pages/StartUpPage.java
@@ -4,24 +4,27 @@ package com.example.teamcity.ui.pages;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import com.example.teamcity.ui.Selectors;
 import lombok.Getter;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.element;
 @Getter
 public class StartUpPage extends Page {
-    private SelenideElement header = element($("h1#header"));
-    private SelenideElement acceptLicense = element("input[id='accept']");
-    private SelenideElement restoreFromBackupButton = element("input[id='restoreButton']");
-    private SelenideElement proceedButton = element("input[id='proceedButton']");
-    private SelenideElement continueButton = element("input[name='Continue']");
-
+    private SelenideElement createAdminAccountHeader = element(Selectors.byId("header"));
+    private SelenideElement header = element(Selectors.byId("header"));
+    private SelenideElement acceptLicense = element(Selectors.byId("accept"));
+    private SelenideElement proceedButton = element(Selectors.byId("proceedButton"));
+    private SelenideElement submitButton = element(Selectors.byType("submit"));
 
     public StartUpPage open(){
-        Selenide.open("/");
+        Selenide.open("/mnt");
         return this;
+    }
+
+    public SelenideElement getHeader() {
+        return createAdminAccountHeader.shouldBe(Condition.visible, Duration.ofSeconds(35));
     }
 
     public StartUpPage setupTeamCityServer(){
@@ -30,11 +33,13 @@ public class StartUpPage extends Page {
         waitUntilPageIsLoaded();
         proceedButton.click();
         waitUntilPageIsLoaded();
+        header.shouldBe(Condition.visible, Duration.ofMinutes(5));
         acceptLicense.shouldBe(Condition.enabled, Duration.ofMinutes(5));
         acceptLicense.scrollTo();
         acceptLicense.click();
-        continueButton.click();
+        submitButton.click();
         return this;
+
     }
 
 }
